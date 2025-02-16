b67b084233cc245c38f8fd4411f70102302c737e
elise-p <elise.ponimangit@gmail.com>
2025-02-15 20:57:07 -0800

    added coco download code & res

---
+++ coco-download/coco_download_1.py
@@ -0,0 +1,13 @@
+import fiftyone
+# from pycocotools import COCO
+
+dataset = fiftyone.zoo.load_zoo_dataset(
+    "coco-2017",
+    split="validation",
+    label_types=["detections", "segmentations"],
+    classes=["person", "car"],
+    max_samples=50,
+)
+
+# Visualize the dataset in the FiftyOne App
+session = fiftyone.launch_app(dataset)
